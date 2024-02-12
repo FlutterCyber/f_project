b0b06601fbab245db0018ffb0da024d0df8964e5
fluttercyber <ronaldrask1@gmail.com>
2024-02-12 22:03:30 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:f_project/home_page.dart';
+import 'package:f_project/second_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+      routes: {
+        HomePage.id: (context) => const HomePage(),
+        SecondPage.id: (context) => const SecondPage(),
+      },
+    );
+  }
+}
